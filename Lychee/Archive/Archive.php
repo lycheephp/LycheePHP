baeddb694e5f3f0848fe92369c402288fda06a4e
koboshi <koboshi1989@gmail.com>
2014-01-18 22:36:53 +0800

    archive module prototype

--- Lychee/Archive/Archive.php
+++ Lychee/Archive/Archive.php
@@ -19,4 +19,103 @@ use Lychee\Base\MySQL\QueryHelper as QueryHelper;
 class Archive
 {
 
+    /**
+     * 文章表查询类
+     * @var QueryHelper
+     */
+    private $archive;
+
+    /**
+     * 文章分类表查询类
+     * @var QueryHelper
+     */
+    private $category;
+
+    /**
+     * 构造器
+     */
+    public function __construct()
+    {
+        $this->archive = new QueryHelper('archive');
+        $this->category = new QueryHelper('archive_category');
+    }
+
+    /**
+     * 添加文章
+     * @param array $data
+     * @return int
+     */
+    public function addArchive(array $data)
+    {
+        return $this->archive->data($data)->insert();
+    }
+
+    /**
+     * 添加文章分类
+     * @param array $data
+     * @return int
+     */
+    public function addCategry(array $data)
+    {
+        return $this->category->data($data)->insert();
+    }
+
+    /**
+     * 编辑文章
+     * @param array $data
+     * @param int $id
+     * @return int
+     */
+    public function editArchive(array $data, $id)
+    {
+        $id = intval($id);
+        if ($id < 1) {
+            return 0;
+        }
+        return $this->archive->where(array('archive_id' => $id))->data($data)->update();
+    }
+
+    /**
+     * 编辑文章分类
+     * @param array $data
+     * @param int $id
+     * @return int
+     */
+    public function editCategory(array $data, $id)
+    {
+        $id = intval($id);
+        if ($id < 1) {
+            return 0;
+        }
+        return $this->category->where(array('archive_id' => $id))->data($data)->update();
+    }
+
+    /**
+     * 删除文章
+     * @param int $id
+     * @return int
+     */
+    public function deleteArchive($id)
+    {
+        $id = intval($id);
+        if ($id < 1) {
+            return 0;
+        }
+        return $this->archive->where(array('archive_id' => $id))->delete();
+    }
+
+    /**
+     * 删除文章分类
+     * @param int $id
+     * @return int
+     */
+    public function deleteCategory($id)
+    {
+        $id = intval($id);
+        if ($id < 1) {
+            return 0;
+        }
+        return $this->category->where(array('archive_id' => $id))->delete();
+    }
+
 }

d06627e73605b6b48020be82c49a72389df0e330
koboshi <koboshi1989@gmail.com>
2014-01-22 11:41:18 +0800

    developing

---
+++ Lychee/Admin/AdminSystem.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright 2013 henryzengpn koboshi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace Lychee\Admin;
+
+/**
+ * 后台管理系统逻辑类
+ * @author Samding
+ * @package Lychee\Admin
+ */
+class AdminSystem
+{
+
+    /**
+     * 构造器
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 以树状图方式获取菜单
+     */
+    public function getMenuTree()
+    {
+
+    }
+
+    /**
+     * 检查权限
+     */
+    public function checkPrivilege()
+    {
+
+    }
+
+    /**
+     * 分配权限
+     */
+    public function assignPrivilege()
+    {
+
+    }
+
+    /**
+     * 移除后台管理菜单
+     */
+    public function removeMenu()
+    {
+
+    }
+
+}
